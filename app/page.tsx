3ace6429dad4809eb10bec7b7c008acd8e67872b
ignaarguello <igna.arguello2110@hotmail.com>
2023-04-26 19:16:04 -0300

    last commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import './styles/styles.css'
+import Slider from './components/Slider'
 
 export default function Home() {
   return (
     <div id='container-home'>
+      <Slider/>
     </div>
   )
 }
